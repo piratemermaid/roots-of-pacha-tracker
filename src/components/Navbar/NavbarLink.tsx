a601b59621ed42c70b19e8e3a2e94668f26797d9
Ariel <thepiratemermaid4@gmail.com>
2024-01-31 15:36:45 -0700

    Style navbar links

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom';
-import { Grid } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import { Button, Grid } from '@mui/material';
 
 type Props = { name: string; label: string };
 
 export default function NavbarLink({ name, label }: Props) {
+  const location = useLocation();
+  const isActive = location.pathname.slice(1) === name;
+
   return (
     <Grid item>
-      <Link to={name}>{label}</Link>
+      <Link to={name}>
+        <Button
+          variant="contained"
+          size="small"
+          sx={{ px: 1, py: 0.5 }}
+          color={isActive ? 'primary' : 'secondary'}
+        >
+          {label}
+        </Button>
+      </Link>
     </Grid>
   );
 }
