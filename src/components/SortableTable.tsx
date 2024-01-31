449368922949993d8eb7543463b1f22e73ff0caa
Ariel <thepiratemermaid4@gmail.com>
2024-01-31 13:26:21 -0700

    Make table small

--- src/components/SortableTable.tsx
+++ src/components/SortableTable.tsx
@@ -18,39 +18,41 @@ type Props = {
 
 export default function SortableTable({ headCells, rows }: Props) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="table">
-        <TableHead>
-          <TableRow>
-            {headCells.map((headCell) => (
-              <TableCell
-                sx={{ fontWeight: 600, color: 'var(--color-secondary)' }}
-              >
-                {headCell.label}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => {
-            return (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                {headCells.map((headCell) => (
-                  <TableCell
-                    component={headCell.id === 'name' ? 'th' : 'td'}
-                    sx={headCell.id === 'name' ? { fontWeight: 600 } : null}
-                  >
-                    {row[headCell.id]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="table" size="small">
+          <TableHead>
+            <TableRow>
+              {headCells.map((headCell) => (
+                <TableCell
+                  sx={{ fontWeight: 600, color: 'var(--color-secondary)' }}
+                >
+                  {headCell.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => {
+              return (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  {headCells.map((headCell) => (
+                    <TableCell
+                      component={headCell.id === 'name' ? 'th' : 'td'}
+                      sx={headCell.id === 'name' ? { fontWeight: 600 } : null}
+                    >
+                      {row[headCell.id]}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
