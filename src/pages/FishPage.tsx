da2f5638c868f015ac97bdfd34091d3cd3c35a8c
piratemermaid <thepiratemermaid4@gmail.com>
2024-02-14 19:30:56 -0500

    Add fish images

--- src/pages/FishPage.tsx
+++ src/pages/FishPage.tsx
@@ -1,5 +1,28 @@
+import { Grid } from '@mui/material';
+
 import PageLayout from '../components/PageLayout';
+import FISH from '../data/fish';
 
 export default function FishPage() {
-  return <PageLayout title="Fish">FishPage TODO</PageLayout>;
+  return (
+    <PageLayout title="Fish">
+      <Grid container spacing={2} sx={{ mt: 4 }}>
+        {FISH.map((fish) => (
+          <Grid item key={fish.name}>
+            <img
+              className="grayed-out-img"
+              src={`/images/fish/${fish.name}.webp`}
+              style={{
+                position: 'relative',
+                height: 50,
+                width: 50,
+                bottom: 3,
+                left: -4,
+              }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </PageLayout>
+  );
 }
